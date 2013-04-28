9f795c675496399ded7a39be672c938a49d5ca24
Kyle Smyth <kylesmyth@gmail.com>
2013-04-27 21:06:52 -0600

    Explosion particle system

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class BombController : MonoBehaviour 
 {
+	[SerializeField]
+	private GameObject explosion;
 	
 	void OnTriggerEnter(Collider c)
 	{
 		if(c.tag == "Player")
 		{
-			Debug.Log("BOMB DETONATED");	
+			this.Explode();
 		}
+	}
+	
+	private void Explode()
+	{
+		GameObject.Instantiate(
+			this.explosion,
+			this.transform.position,
+			Quaternion.identity
+			);
+		Destroy(this.gameObject);
 	}
 }
